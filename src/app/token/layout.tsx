434733d757cc41562b85bbde30829db6541a7709
SHENMENG <shengmeng0214@shopzpin.com>
2024-11-19 10:06:21 +0800

    create token details folder

---
+++ src/app/token/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// const queryClient = new QueryClient();
+
+export default function TokenLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      {/* <QueryClientProvider client={queryClient}> */}
+        {children}
+      {/* </QueryClientProvider> */}
+    </>
+  );
+}
