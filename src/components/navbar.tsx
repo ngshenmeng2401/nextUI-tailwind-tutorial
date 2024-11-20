af629cfb9b04c372850dc29df5038a5a0515b003
SHENMENG <shengmeng0214@shopzpin.com>
2024-11-20 10:27:03 +0800

    enhance the navbar component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,39 +1,60 @@
+"use client";
 import {
   Navbar,
   NavbarBrand,
   NavbarContent,
   NavbarItem,
   Link,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
 } from "@nextui-org/react";
 import siteLogo from "../../public/images/pump_tube_logo_white.png";
 import { navbarItemList } from "@/config/navbar";
 import { useTranslations } from "next-intl";
 import LanguageSwitcher from "./LanguageSwitcher";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function NavBarComponent() {
   const lang = useTranslations();
-  
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <Navbar>
-      <NavbarBrand>
-        <Image
-          width={100}
-          height={80}
-          alt="logo"
-          src={siteLogo}
-        />
-      </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        {navbarItemList.map((item) => (
-          <NavbarItem key={item.key} className="hidden lg:flex">
-            <Link href={item.url} style={{color: "white"}}>{lang(item.label)}</Link>
-          </NavbarItem>
+    <div>
+      <Navbar maxWidth="xl" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent className="sm:hidden" justify="start">
+        <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} />
+      </NavbarContent>
+        <NavbarContent className="pr-3" justify="center">
+          <NavbarBrand hidden={true}>
+            <Image width={100} height={80} alt="logo" src={siteLogo} />
+          </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent className="sm:flex gap-4" justify="start">
+          {navbarItemList.map((item) => (
+            <NavbarItem key={item.key} className="hidden lg:flex">
+              <Link href={item.url} style={{ color: "white" }}>
+                {lang(item.label)}
+              </Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+        <NavbarMenu style={{backgroundColor:"black"}}>
+        {navbarItemList.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link href={item.url} style={{ color: "white" }}>
+                {lang(item.label)}
+              </Link>
+          </NavbarMenuItem>
         ))}
-      </NavbarContent>
-      <NavbarContent justify="end">
-        <LanguageSwitcher/>
-      </NavbarContent>
-    </Navbar>
+      </NavbarMenu>
+        <NavbarContent justify="end">
+          <LanguageSwitcher />
+        </NavbarContent>
+      </Navbar>
+    </div>
   );
 }
